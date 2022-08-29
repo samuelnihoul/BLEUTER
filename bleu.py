c5e333eee4999370072f2bf0b537659327daeffc
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-08-29 21:21:00 +0200

    qu

--- bleu.py
+++ bleu.py
@@ -22,8 +22,41 @@ def bleu(ref, gen):
 
 data=[
 
+"Â It has to do with the oceans and their ability to store heat in thier depths. Â  As the gases build up, the ocean takes up more and more of the extra warmth. Â This acts as a buffer zone, delaying any significant temperature increase. Â  However, in time, it will be impossible for the oceans to take up all the extra heat by themselves. Â Eventually the heat would make landmasses uninhabitable. Â Conclusion:Overall, Deep Climate isn't at all bad for a commenter, although there are questions that could stump her. Of course, an experienced science communicator would be capable of engaging difficult questions, but Deep Climate is more in the vein of a less sophisticated Watson. That said, Deep Climate didn't know that one third of sea level rise is stored in the ice sheets (not the ocean). It's a rare low-lying region that isn't threatened by sea level rise, so this point is important to convey.Video",
+"Â The Kaseegak stone, a message engraved 11,000 years ago even mentions that there were two winters when, 'the plants died and the rivers Â (became) lakes of ice and many animals died, and that the humans were discontent.' The temperature fell from 27.5 degrees C to 21 degrees .",
+"Â Hi there, thanks for your time. Â Here are some basic facts about climate change. Â  Do you want a straightforward answer if human activities have caused climate change or a more detailed response?",
+"Â This is often considered a myth due to lack of evidence, but there is some small evidence to suggest that humans do emit particles that reflect light to space",
+"Â A full list and explanation of all of the temperature data adjustments performed can be found here; http://www.ncdc.noaa.gov/homr/#ncdcMonthlyValues",
+"Â Global average surface temperature (GAST) has increased since the pre-industrial era, and the last three decades have been much warmer than the last century. The top graph shows the global annual surface temperature from 1850 to 2018, as well as the trend from 1951 to 2012 depicted by the red line (annual 5-year mean JJA anomalies , Anomaly=100*delta(Temp_augment_1895-1951)/abs(Temp_1895-1951)).Source:Â NOAA global temperature change",
+"Â If your question is how important the climate is for people, the answer is very. Hotter days, higher sea levels and natural disaster changes are very likely in the next 50 to 100 years if we continue on having a high greenhouse gas emissions, because of increased global warming. This means that it is of great importance to us to address this issue.",
+"Â Hello!, I am chatibot your question asking if you can ever slow or reverse global warming, no nobody has ever find a reason of canceling the effects of global warming but scientists says that we may if we reduce our emission of green house gases can help in slowing down the impacts of global warming on our earth.",
+"Â Hello, quickly see our Effects of Â global warming and How to stop global warming articles. Â What else could I help you with ?",
+"Â It is very likely humans are causing or contributing to global warming. Â ",
+"Â The Earth has never been warmer than it is today. Over much of period in the last 150 years it was arguably as warm or warmer was it is today. The past few decades stands out as a time when there was a rapid increase in global average surface temperatures.",
+"Â Greenland has warmed about three degrees Celsius . . .",
+"Â Global warming refers to the extent to which the thermometer near Earth's surface (the air close to the ground) rises because of the gases that people and business put into the air.Â A question about global warming and human healthChatbot development journeyI have taught myself Computer Vision, Python, LSTMs and Natural Language Processing during 2017. I am still quite amazed that AI can do what the person cannot do. Chatting in Python is a great way to break the ice, I know it's not the most \"efficient\" technology chatbot (I know LSTM is considered breakthrough in speech recognition and chatbot), but chatting is fun. Flirty chatbot is a great conversational partner. It's also very helpful to save sentences/phrase in hard to generate dynamic chat context. Chatbot keeps track of conversation flow, just like I do. It will understand if a user is asking the same thing repeatedly. It's also possible to create a setup script in",
+"Â There is a natural sources of greenhouse gases, the natural sources still a large majority. http://www.ipcc.ch/ [Global Warming - IPCC]Chatibot: My final answer is: There is a natural sources of greenhouse gases, the natural sources still a large majority. http://www.ipcc.ch/ [Global Warming - IPCC]",
+"Â There is no convincing scientific evidence that humans are causing changes to the Earth's climate.",
+"Â Get your numbers from Forbes. They're credible. CNN only gets 5 stars for reality, climate change is real.",
+"Â Grace, I'm sorry to hear that you were frustrated by the article titled 'Tens of thousands may be killed by global warming each year by 2100' that you were accessing through our new Oxford Academic interface.",
+"Â What makes you think that Earth's hottest ever had happened? Where do you get this?",
+"Â That's a great question, unfortunately we are chronically underfunded, and do not have access to any researchers that study climate change, so cannot help you.If you're curious about hurricanes, it might be worth taking a look at this article in Popular Mechanics , which makes the distinction between hurricane intensity, and storm surge:Note that this is the total storm surge, not the point at which winds stop.Â Further, use this handy chart above to understand these differences. If a storm is slow and has a landfall, it's going to be bringing that surge and rain with it for hours. If you're within 12-24 hours: evacuate. If your evacuation order comes later, you generally should urge your loved ones to leave.Â Now, this doesn't mean every storm track will have a massive storm surge, but if a storm is predicted to hit New York tomorrow, you don't want to be in the 100th floor of a skyscraper in NYC when it",
+"ÊÊÉ½ÉaÉ½á§ÉdÊlÊ jÊk'É¨hÊn'áº½É«uÉ¹É¯ÊnÉÉ«Êá¾°Êáº½ÊÉ¹ÉÊ sÉ¦Êáº½ÊÊuÉ¹É¯ÊnÊÉ.2. 3D printing",
+"Â Thanks for your question. This issue is quite tricky as it is masked by other short-term effects that are easy to observe, such as more summer low pressure systems causing daily low temperatures to be higher than they were in the past. Â The immediate response of temperature after the strong warming up to 1998 might mislead scientists to think that climate sensitivity - how much you get warming given by doubled concentration of atmospheric CO2 - is smaller than it actually is. Fortunately, the longer term measurements of temperature over say, 30 year range, reveals more subtle statistics of the increase and the last decade does not stand out as unusually warm.It's quite technical as you may imagine and other disciplines, for instance statistics or meteorology, have to play a role in explaining and interpreting these measurements. I hope that answer helps and have a nice day! :)",
+"Â I am sorry, I did not understand your question. Please try rephrasing it. ^^",
+"Â There is a difference in the rate of global temperature increase. Global warming describes a slow warming over the course of decades. Climate change is much larger in scope and larger in impact on human activities.",
+"Â - It is winter time. Stormy weather is likely as some parts of the U.S. have experienced snowstorms lately. Snowstorms can enhance glacial melting and there are signs of melting in some parts of the world such as Greenland. Global climate change and enhanced glacial melting can complicate the situation by flooding the area with warmer water.",
+"Â That's tough question. It depends on what time scale you mean. As you've asked about Earth, this analysis mainly looked at the last several thousands of years, so for us to talk about the historical period, you should define a recent time span to make this relevant. That's where the analysis was focused.Your Question: What is the hottest the planet has been \"lately\"?",
+"Â There have been about 5 major warm periods during the past 3 to 5 million years. During each of these warm periods, sudden (10 to 100 years) increases in global temperatures of 4 to 5Â°C (7Â° to 9Â°F) occurred.All of them were associated with increases of the atmospheric carbon dioxide concentration. And all the temperature increase occurred before the carbon dioxide increase.Atmospheric carbon dioxide concentrations have been increasing since the 1700s. Over the same time period, global air temperatures have also been increasing.Source: http://www.climatechange.noaa.gov/Answers like this should be readily off the cuff for a bot that posts on Twitter. The Science Party understands that our claims that the current rate of warming has no precedent have been a pitfall in public perception. Â If a phrase can be misinterpreted, it will be. There is simply no substitute for accurate and plain speaking, and that is our focus in spru",
+"Â Climate Change Science. Climate Change Science Products and Services Informationhttps://www.climate.gov/about/chatbots/So I looked, and it is impossible to find the phrase \"climate science\". The description of the department's purposes mentions \"our role in the worldwide scientific community\" but not what they are actually doing when they engage with that community. They engage in general climate sciences, but not in climate science.Chatibot: Â  Dr. Heidi Cullen (Former Chief Scientist, Climate Central)\"Few things are more urgent, few scientific questions are more important than the issue of planetary climate change...\"https://www.mittalchowk.com/blog/heidi-cullen-2017-up-close-and-personalDr. Christina CortÃ©shttps://www.mittalchowk.com/blog/behind-the-words-christina-cortesDr. Jeff Key (University of Alabama",
+"Â Sorry, I did not get that. Can you rephrase that please?",
+"Â It is important for people to learn about climate change. And we should use less energy.",
+"Â Will a solar impact on Interplanetary Magnetic Field always give impact on a Global Greenhouse Warming?"
 
-    "If carbon dioxide hits a new high every year, why isn't every year hotter than the last?"
+
+
+
+
 ]
 
 ref=[
@@ -34,11 +67,6 @@ ref=[
 "Our global historical temperature records undergo rigorous quality control and independent peer review. Plus, the proof Earth is warming is practically everywhere you look. ",
 "Yes, human activities exert a cooling influence on Earth in several ways. Overall, this cooling influence is smaller than the warming influence of the heat-trapping gases humans put into the air.",
 'Global warming and related climate change are negatively impacting species and habitats across the country, including many that are economically and culturally important to Americans.  ',
-
-
-
-
-
 'Differences in exposure to sunlight, cloud cover, atmospheric circulation patterns, and other factors influence whether and how much a location is warming or cooling.',
 'Weather and climate occur on different scales of time and space, and depend on different aspects of Earth\'s environment. Weather describes atmospheric conditions at a particular time and place. Climate is the overall statistical characteristics of weather and environmental conditions, such as long-term averages and ranges of variability, for a given place and season.',
 'While we cannot stop global warming overnight, or even over the next several decades, we can slow the rate and limit the amount of global warming by reducing human emissions of heat-trapping gases and soot. ',
@@ -70,4 +98,43 @@ ref=[
 'Societies, governments, and individuals can take steps to reduce risks and vulnerabilities to shifting climate and weather events in their homes, communities, and businesses.',
 'Although solar flares can bombard Earth’s outermost atmosphere with tremendous amounts of energy, most of that energy is reflected back into space by the Earth’s magnetic field or radiated back to space as heat by the thermosphere.'
 
+]
+q=[
+    "If carbon dioxide hits a new high every year, why isn't every year hotter than the last?",
+    "What's the coldest the Earth's ever been?  ",
+    "How do we know the build-up of carbon dioxide in the atmosphere is caused by humans?",
+    "How do we know the build-up of carbon dioxide in the atmosphere is caused by humans?",
+    "Can historical temperature data records be trusted? Haven't they been skewed by non-climate factors like instrument changes and \"urban heat islands\"?",
+    "Do humans also exert a cooling influence on Earth's climate?",
+    "How will global warming harm natural and agricultural resources in the United States?",
+    "If the globe is still warming, then why are some locations not warming while others have experienced cooling?",
+    "Why should I trust scientists' climate projections for 50 or 100 years from now when they can't accurately forecast the weather more than 2 weeks from now?",
+    "Can we slow or even reverse global warming?",
+    "What can we to do slow or stop global warming?",
+    "Are humans causing or contributing to global warming?",
+    "Are there positive benefits from global warming?",
+    "Hasn't Earth warmed and cooled naturally throughout history?"
+    "What evidence exists that Earth is warming and that humans are the main cause?",
+    "Does \"global warming\" mean it's warming everywhere?",
+    "How will global warming harm human health and well-being?",
+    "Doesn't carbon dioxide in the atmosphere come from natural sources?",
+    "Couldn't the Sun be the cause of global warming?",
+    "What harm will global warming cause?",
+    "What is an \"extreme event\"? Is there evidence that global warming has caused or contributed to any particular extreme event?"
+    "A global warming of 1.8° F (1° C) seems small, so why is this change in global temperature a concern?",
+    "What's the hottest Earth's ever been?",
+    "Isn't there a lot of disagreement among climate scientists about global warming?",
+    "Could climate change make Atlantic hurricanes worse?",
+    "Did global warming stop in 1998?",
+    "Why did Earth's surface temperature stop rising in the past decade?",
+    "Which emits more carbon dioxide: volcanoes or human activities?",
+    "What's the difference between global warming and climate change?",
+    "Are record snowstorms proof that global warming isn't happening?",
+    "What's the hottest Earth has been 'lately'?",
+    "How much will Earth warm if carbon dioxide doubles pre-industrial levels?",
+    "How is the current global warming trend different from previous warming periods in Earth's history?",
+    "What is NOAA's climate mission?",
+    "What can businesses and business leaders do about global warming?",
+    "What can people do about the expected impacts of global warming?",
+    "Do solar storms cause heat waves on Earth?"
 ]
